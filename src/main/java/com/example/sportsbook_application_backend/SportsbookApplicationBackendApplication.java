97112cba7861b19167f3afde9e7777ecd0667c63
yordanov0502 <yordanovtodor281@gmail.com>
2023-03-13 14:20:03 +0200

    comment added

--- src/main/java/com/example/sportsbook_application_backend/SportsbookApplicationBackendApplication.java
+++ src/main/java/com/example/sportsbook_application_backend/SportsbookApplicationBackendApplication.java
@@ -8,6 +8,7 @@ public class SportsbookApplicationBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportsbookApplicationBackendApplication.class, args);
+        //
     }
 
 }
