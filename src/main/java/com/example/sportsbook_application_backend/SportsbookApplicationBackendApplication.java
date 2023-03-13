b7684d3867f5c1954cea72eef07099a57bd86420
yordanov0502 <yordanovtodor281@gmail.com>
2023-03-13 13:56:58 +0200

    Initial project

---
+++ src/main/java/com/example/sportsbook_application_backend/SportsbookApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsbook_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsbookApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsbookApplicationBackendApplication.class, args);
+    }
+
+}
