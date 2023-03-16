d2c1d0f491b4cf5c20c0e190d3515946f548cc9f
tedonikolov <teonikolov01@abv.bg>
2023-03-16 09:42:15 +0200

    Created UserController with registration endpoint.

---
+++ src/main/java/com/example/sportsbook_application_backend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.sportsbook_application_backend.controller;
+
+import com.example.sportsbook_application_backend.model.dto.UserDTO;
+import com.example.sportsbook_application_backend.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registration")
+    public String registration(@RequestBody UserDTO userDTO){
+        if(userService.createUser(userDTO))
+            return "User created successful!";
+        else
+            return "User with such credentials exists!";
+    }
+}
