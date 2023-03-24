19a3a62845da979a5ee31980b121cf900c98408b
Todor Yordanov <yordanovtodor281@gmail.com>
2023-03-24 06:01:22 +0000

    Bet service created.

--- src/main/java/com/example/sportsbook_application_backend/repository/EventRepository.java
+++ src/main/java/com/example/sportsbook_application_backend/repository/EventRepository.java
@@ -3,5 +3,9 @@ package com.example.sportsbook_application_backend.repository;
 import com.example.sportsbook_application_backend.model.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public interface EventRepository extends JpaRepository<Event,Long> {
+    ArrayList<Event> getAllByDate(LocalDate localDate);
 }
