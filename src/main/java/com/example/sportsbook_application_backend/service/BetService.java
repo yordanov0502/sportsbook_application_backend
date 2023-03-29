92b4e524b5b69dbf3dbbe1dd8b2af0be8a3aa5e7
yordanov0502 <yordanovtodor281@gmail.com>
2023-03-29 23:13:56 +0300

    Bet service updated.

--- src/main/java/com/example/sportsbook_application_backend/service/BetService.java
+++ src/main/java/com/example/sportsbook_application_backend/service/BetService.java
@@ -1,6 +1,5 @@
 package com.example.sportsbook_application_backend.service;
 
-import com.example.sportsbook_application_backend.exception.FieldException;
 import com.example.sportsbook_application_backend.exception.NonexistentDataException;
 import com.example.sportsbook_application_backend.model.dto.odd.OddDTO;
 import com.example.sportsbook_application_backend.model.dto.odd.OddResultDTO;
@@ -25,38 +24,43 @@ public class BetService {
     private EventService eventService;
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private LeagueService leagueService;
-    @Autowired
-    private UserService userService;
 
     public int callAPIForOddsByDate(String date) {
         int numberOfFixtures=0;
 
         ArrayList<Event> events = eventService.getAllFixturesByDate(date);
-        Map<Long, OddDTO> odds = new HashMap<>();
 
-        for (League league : leagueService.getAllowedLeagues()) {
-            //adding all odds for specific date and league into one list
-            List<OddDTO> oddDTOList = restTemplate.getForObject("/odds?league=" + league.getId() + "&season=" + league.getSeason() + "&date=" + date + "&bookmaker=8&bet=1", OddResultDTO.class).getResponse();
-            for (OddDTO oddDTO : oddDTOList) {
-                odds.put(oddDTO.getFixture().getId(), oddDTO);
-                numberOfFixtures++;
-            }
+        if(events.isEmpty()) throw new NonexistentDataException("No fixtures found in database for date:"+date);
+
+        //map for storing id and season only of ACTIVE leagues for a specific date
+        Map<Long,Integer> activeLeagues = new HashMap<>();
+        for(Event event: events)
+        {
+            activeLeagues.put(event.getLeague().getId(),event.getLeague().getSeason());
         }
 
-        for (Event event : events) {
-            if (odds.containsKey(event.getId())) {
-                Bet betHome = new Bet(null, event, Outcome.PENDING, ResultType.ONE, Float.parseFloat(odds.get(event.getId()).getBookmakers().get(0).getBets().get(0).getValues().get(0).getOdd()));
-                Bet betDraw = new Bet(null, event, Outcome.PENDING, ResultType.ZERO, Float.parseFloat(odds.get(event.getId()).getBookmakers().get(0).getBets().get(0).getValues().get(1).getOdd()));
-                Bet betAway = new Bet(null, event, Outcome.PENDING, ResultType.TWO, Float.parseFloat(odds.get(event.getId()).getBookmakers().get(0).getBets().get(0).getValues().get(2).getOdd()));
+        for(Map.Entry<Long,Integer> l: activeLeagues.entrySet())
+        {
+            //adding all odds for specific date and active league into one list
+            List<OddDTO> oddDTOList = restTemplate.getForObject("/odds?league=" + l.getKey() + "&season=" + l.getValue() + "&date=" + date + "&bookmaker=8&bet=1", OddResultDTO.class).getResponse();
+            for (OddDTO oddDTO : oddDTOList)
+            {
+                if(eventService.fixtureExists(oddDTO.getFixture().getId()))
+                {
+                    Event event = eventService.getFixtureById(oddDTO.getFixture().getId());
 
-                betRepository.save(betHome);
-                betRepository.save(betDraw);
-                betRepository.save(betAway);
+                    Bet betHome = new Bet(null, event, Outcome.PENDING, ResultType.ONE, Float.parseFloat(oddDTO.getBookmakers().get(0).getBets().get(0).getValues().get(0).getOdd()));
+                    Bet betDraw = new Bet(null, event, Outcome.PENDING, ResultType.ZERO, Float.parseFloat(oddDTO.getBookmakers().get(0).getBets().get(0).getValues().get(1).getOdd()));
+                    Bet betAway = new Bet(null, event, Outcome.PENDING, ResultType.TWO, Float.parseFloat(oddDTO.getBookmakers().get(0).getBets().get(0).getValues().get(2).getOdd()));
+
+                    betRepository.save(betHome);
+                    betRepository.save(betDraw);
+                    betRepository.save(betAway);
+
+                    numberOfFixtures++;
+                }
             }
         }
-
         return numberOfFixtures;
     }
 
