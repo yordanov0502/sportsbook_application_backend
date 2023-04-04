50cac22b08e1fb81afa81d4595655ac843f6a7c1
tedonikolov <teonikolov01@abv.bg>
2023-04-04 10:23:17 +0300

    Created Authentication Filter and Updates in SecurityConfig

--- src/main/java/com/example/sportsbook_application_backend/controller/UserController.java
+++ src/main/java/com/example/sportsbook_application_backend/controller/UserController.java
@@ -4,15 +4,14 @@ import com.example.sportsbook_application_backend.config.JwtService;
 import com.example.sportsbook_application_backend.model.dto.user.UserChangePasswordDTO;
 import com.example.sportsbook_application_backend.model.dto.user.UserDTO;
 import com.example.sportsbook_application_backend.model.mapper.*;
-import com.example.sportsbook_application_backend.model.dto.user.UserLoginDTO;
 import com.example.sportsbook_application_backend.model.dto.user.UserRegistrationDTO;
 import com.example.sportsbook_application_backend.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
@@ -40,19 +39,17 @@ public class UserController {
         return userMapper.mapToUserDTO(userService.getUserByUsername(userRegistrationDTO.getUsername()));
     }
 
-
     @PostMapping("/login")
-    public UserDTO userLogin(@RequestBody UserLoginDTO userLoginDTO, HttpServletResponse httpServletResponse){
-
-        userService.validateLoginFields(userLoginDTO);
-        userService.validateUser(userLoginDTO.getUsername(),userLoginDTO.getPassword());
-
-        httpServletResponse.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwtService.generateToken(userService.getUserByUsername(userLoginDTO.getUsername())));
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-
-        return userMapper.mapToUserDTO(userService.getUserByUsername(userLoginDTO.getUsername()));
+    public UserDTO userLogin(HttpServletRequest request){
+        Principal principal = request.getUserPrincipal();
+        return userMapper.mapToUserDTO(userService.getUserByUsername(principal.getName()));
     }
 
+    @GetMapping()
+    public UserDTO getProfile(HttpServletRequest request){
+        Principal principal= request.getUserPrincipal();
+        return userMapper.mapToUserDTO(userService.getUserByUsername(principal.getName()));
+    }
 
     @PostMapping("/edit/information")
     public UserDTO editInformation(@RequestBody UserDTO userDTO, HttpServletResponse httpServletResponse) {
