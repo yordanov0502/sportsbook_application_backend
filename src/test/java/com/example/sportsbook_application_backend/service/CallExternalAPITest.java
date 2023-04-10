37b45ec62d4c1a3ae1b325f867c31b74923474f7
yordanov0502 <yordanovtodor281@gmail.com>
2023-04-10 17:09:12 +0300

    annotation added

--- src/test/java/com/example/sportsbook_application_backend/service/CallExternalAPITest.java
+++ src/test/java/com/example/sportsbook_application_backend/service/CallExternalAPITest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RootUriTemplateHandler;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplateHandler;
 
@@ -21,6 +22,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestPropertySource({ "/application-test.properties" })
 @DataJpaTest
 class CallExternalAPITest {
 
