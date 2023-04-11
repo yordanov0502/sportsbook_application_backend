412255b9c8ce80a160df7840096e360a9db222a7
yordanov0502 <yordanovtodor281@gmail.com>
2023-04-11 11:59:10 +0300

    yml updated

--- src/test/java/com/example/sportsbook_application_backend/SportsBookBackendApplicationTests.java
+++ src/test/java/com/example/sportsbook_application_backend/SportsBookBackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.sportsbook_application_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY, connection = EmbeddedDatabaseConnection.H2)
 class SportsBookBackendApplicationTests {
 
     @Test
