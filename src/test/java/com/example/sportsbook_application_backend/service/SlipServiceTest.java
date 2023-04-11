4425f762be41210aa5ba6e72602468c13c08ac03
yordanov0502 <yordanovtodor281@gmail.com>
2023-04-11 11:53:23 +0300

    yml updated

--- src/test/java/com/example/sportsbook_application_backend/service/SlipServiceTest.java
+++ src/test/java/com/example/sportsbook_application_backend/service/SlipServiceTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,6 +33,7 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY, connection = EmbeddedDatabaseConnection.H2)
 class SlipServiceTest {
     @MockBean
     private SlipRepository slipRepository;
