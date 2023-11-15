68470a1e846867f240a54362ffac124fc1d28a54
yordanov0502 <yordanovtodor281@gmail.com>
2023-11-15 16:43:19 +0200

    Rapid API key secured

--- src/main/java/com/example/sportsbook_application_backend/config/RestTemplateConfig.java
+++ src/main/java/com/example/sportsbook_application_backend/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.example.sportsbook_application_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +11,16 @@ import org.springframework.web.util.UriTemplateHandler;
 
 @Configuration
 public class RestTemplateConfig {
+
+    @Value("${spring.rapid.api.key}")
+    private String RAPID_API_KEY;
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         UriTemplateHandler uriTemplateHandler = new RootUriTemplateHandler("https://api-football-v1.p.rapidapi.com/v3/");
         return builder
                 .uriTemplateHandler(uriTemplateHandler)
-                .defaultHeader("X-RapidAPI-Key","6e113cdd7dmsha58c67d494ccc13p19c311jsn1a974ed0bd51")
+                .defaultHeader("X-RapidAPI-Key",RAPID_API_KEY)
                 .defaultHeader("X-RapidAPI-Host","api-football-v1.p.rapidapi.com")
                 .build();
     }
